867873c0b608cad2b13d88d0acfa8f1db651d795
SoniaFerreyra <126492553+SoniaFerreyra@users.noreply.github.com>
2023-03-06 12:14:40 -0300

    Funcion

--- Clase 4/src/Main.java
+++ Clase 4/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Clase 4");
+        System.out.println("Clase 4: Funciones");
+
+        Sumar();
+
+
+    }
+    public static int Sumar(){
+        int x = 2 ;
+        int i = 4;
+        int resultado = x + i;
+        System.out.println(resultado);
+        return resultado;
     }
 }
